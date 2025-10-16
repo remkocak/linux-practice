628d5c6764b9f37a4183516d622bd8811470037a
remkocak <102284871+remkocak@users.noreply.github.com>
2025-10-16 12:29:55 +0000

    week3maxnum

--- object/week2hw.cpp
+++ object/week2hw.cpp
@@ -11,6 +11,17 @@ void fun(int n){
     cout<<n<<endl;
 }
 
+int* maxnum_address(int *a, int n){
+    int* maxpointer = &a[0];
+    for(int i =0; i<n; i++){
+        if(a[i]> *maxpointer) {
+            maxpointer=&a[i]; //address için 
+            *maxpointer=a[i]; //value için
+        }
+        return maxpointer;
+    }
+}
+
 int main(int argc, char const *argv[]){
     cout<<argv[0]<<" ";
     int a[10]={0,1,2,300,4,5,6,7,8,9};
@@ -24,5 +35,13 @@ int main(int argc, char const *argv[]){
             maxnum=a[i];
     }
     cout<<maxnum<<endl;
+    cout<<"/n/n"<<endl;
+    cout<<&a[0]<<endl; 
+    // cout<<(a+1) de aynı sonucu verir
+    char b[]={'ef'};
+    cout<<(int)b[0]<<endl; //b[0] yani e'nin integer cinsinden değerini basacak
+
+    maxnum_address(a,10);
+
     return 0;
 }
