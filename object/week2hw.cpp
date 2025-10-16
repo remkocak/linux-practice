a790ffa6dde660c39dee7bc896fa5ff6e0a83de6
remkocak <102284871+remkocak@users.noreply.github.com>
2025-10-16 11:04:26 +0000

    week3devam

--- object/week2hw.cpp
+++ object/week2hw.cpp
@@ -13,5 +13,16 @@ void fun(int n){
 
 int main(int argc, char const *argv[]){
     cout<<argv[0]<<" ";
+    int a[10]={0,1,2,300,4,5,6,7,8,9};
+    for(int i=3; i<10;i++){
+        a[i]+=5;
+        cout<<a[i]<<" ";
+    }
+    int maxnum=a[0];
+    for(int i=1; i<10<i++){
+        if(a[i]>maxnum)
+            maxnum=a[i];
+    }
+    cout<<maxnum<<endl;
     return 0;
 }
